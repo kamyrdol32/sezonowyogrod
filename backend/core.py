03508c6e5b719defb9577b73fa680ae4a44fc9d8
kamyrdol32 <kamyrdol32@gmail.com>
2022-10-13 21:00:50 +0200

    1.0.1

--- backend/core.py
+++ backend/core.py
@@ -1,10 +1,5 @@
 # Flask defaukt app
 from flask import Flask
-from flask import request
-from flask import jsonify
-from flask import render_template
-from flask import redirect
-from flask import url_for
 
 # Flask app
 app = Flask(__name__)
