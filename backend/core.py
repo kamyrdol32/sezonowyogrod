45ac3b27d32147295fcdccd76648f582bde45ed4
kamyrdol32 <kamyrdol32@gmail.com>
2022-10-14 18:47:51 +0200

    1.0.2 Dockers

--- backend/core.py
+++ backend/core.py
@@ -1,5 +1,12 @@
-# Flask defaukt app
 from flask import Flask
 
-# Flask app
 app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
