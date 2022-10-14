f95a966f70aa129e58314b150102baca5d83f1db
Weronika <weronika2241@interia.pl>
2022-10-14 21:49:06 +0200

    1.0.4 docker-compose.yml

---
+++ frontend/src/Home/Home.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+export default function Home() {
+  return (
+    <div>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
