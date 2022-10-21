441eec87e637dfba2321cd9b85e0e58a748da3eb
Weronika <weronika2241@interia.pl>
2022-10-21 22:30:12 +0200

    mapa

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './Home.css';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -14,6 +15,8 @@ import MenuItem from '@mui/material/MenuItem';
 import Grid from '@mui/material/Grid';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Home() {
 
@@ -49,10 +52,10 @@ export default function Home() {
       title: 'Restaurant',
     }, 
   ];
-
+ 
   return (
-    <>
-     <AppBar position="static" style={{background: 'black'}}>
+  <>
+    <AppBar position="static" style={{background: 'black'}}>
       <Container>
         <Toolbar disableGutters>
           <Typography
@@ -173,25 +176,41 @@ export default function Home() {
         </Toolbar>
       </Container>
     </AppBar>
-    <Grid container direction="row" justifyContent="center" sx={{p:6}}>
-        <Grid lg={3} sx={{pr:3, pt:2, pl:3, textAlign: 'justify'}} >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque urna mi, vestibulum ut rutrum eget, lacinia a velit. Nullam hendrerit mi at diam pretium convallis. Vivamus commodo est nisi, ac semper ante vestibulum at. Quisque condimentum sed tellus at hendrerit. Aenean erat dolor, interdum eget dictum vitae, porta sit amet mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi sit amet interdum odio. Suspendisse ut massa elementum, gravida lorem ut, auctor metus. Etiam aliquet mauris ex, vel mattis mi viverra ac. Nam semper risus eu quam convallis tincidunt. Morbi rhoncus neque ligula, sit amet tempus lectus sagittis eu. Pellentesque tincidunt dapibus orci, vitae tristique mi pellentesque vel. Fusce sit amet ultrices magna. Aenean euismod id erat sed malesuada. Ut sit amet lacinia tellus. In ullamcorper sodales mauris, sit amet posuere tortor.
+    <div id="block">
+      <h1>Sezonowy Ogród</h1>
+      <Grid container direction="row" justifyContent="center" sx={{p:4}}>
+          <Grid lg={3} sx={{pr:3, pt:2, pl:3, textAlign: 'justify'}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque urna mi, vestibulum ut rutrum eget, lacinia a velit. Nullam hendrerit mi at diam pretium convallis. Vivamus commodo est nisi, ac semper ante vestibulum at. Quisque condimentum sed tellus at hendrerit. Aenean erat dolor, interdum eget dictum vitae, porta sit amet mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi sit amet interdum odio. Suspendisse ut massa elementum, gravida lorem ut, auctor metus. Etiam aliquet mauris ex, vel mattis mi viverra ac. Nam semper risus eu quam convallis tincidunt. Morbi rhoncus neque ligula, sit amet tempus lectus sagittis eu. Pellentesque tincidunt dapibus orci, vitae tristique mi pellentesque vel. Fusce sit amet ultrices magna. Aenean euismod id erat sed malesuada. 
+          </Grid>
+          <Grid>
+            <ImageList>
+              {photo.map((item) => (
+                <ImageListItem key={item.img} sx={{width: 300}}>
+                  <img
+                    src={`${item.img}`}        
+                    alt={item.title}        
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>  
+          </Grid>
+        </Grid> 
+        <Grid container direction="row" justifyContent="center" sx={{backgroundColor: 'rgba(0,0,0,0.9)', color: 'white', p: 8}}>
+          <Grid>
+            <h3>Kontakt</h3>
+            <div sx={{overflow: 'hidden', maxWidth:'100%', width:'500px' , height: '500px'}}>
+              <div id="my-map-canvas" sx={{height:'100%', width:'100%', maxWidth: '100%'}}>
+                <iframe sx={{height:'100%', width:'100%', border:0}} frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Kraków,+Polska&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8">
+                </iframe>
+              </div>
+            </div>
+          </Grid>
+          <Grid>
+            <a href="https://www.facebook.com/"><FacebookRoundedIcon fontSize="large"/> SezonowyOgrod</a>
+            <a href="https://www.instagram.com/"><InstagramIcon fontSize="large"/> SezonowyOgrod</a>
+          </Grid>      
         </Grid>
-        <Grid>
-           <ImageList>
-            {photo.map((item) => (
-              <ImageListItem key={item.img} sx={{width: 300}}>
-                <img
-                  src={`${item.img}`}        
-                  alt={item.title}        
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>  
-        </Grid>
-      </Grid> 
-      <Box alignItems="stretch" sx={{backgroundColor: 'black', color: 'white', p: 4}}>
-        <h4>Kontakt</h4>
-      </Box>
+    </div>
+    <footer>Weronika Ścibior & Kamil Żegeń &copy; 2022</footer>
   </> 
   );   };
